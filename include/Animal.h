3d96817f427fbf914e4f2833e87feaedea88e0ad
Fiete Wennier <fiete.wennier@adesso.de>
2018-06-13 14:06:23 +0200

    std::string

--- include/Animal.h
+++ include/Animal.h
@@ -1,11 +1,17 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
 
+#include <string>
 
 class Animal {
+    private:
+        const std::string description;
     public:
-        Animal();
+        Animal(std::string description);
         virtual ~Animal();
+
+        virtual void makeNoise() = 0;
+        std::string getDescription();
 };
 
 #endif // ANIMAL_H
--- src/Animal.cpp
+++ src/Animal.cpp
@@ -1,12 +1,17 @@
 #include "Animal.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-Animal::Animal() {
+Animal::Animal(std::string description): description(description) {
     cout << "An animal was born!" << endl;
 }
 
 Animal::~Animal() {
     cout << "An animal has died!" << endl;
+}
+
+std::string Animal::getDescription() {
+    return description;
 }
